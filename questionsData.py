bfa7f45b327d1cd72ce3dfbf4e94dbd5300c7bad
Richard <richard_malagon@soy.sena.edu.co>
2024-03-17 16:39:08 -0500

    prueba richard

--- questionsData.py
+++ questionsData.py
@@ -2,4 +2,8 @@ questions = [{
     "question": "What is the capital of France?",
     "options": ["Paris", "London", "Berlin", "Madrid"],
     "answer": "Paris"
+}, {
+    "question": "What is the capital of France?",
+    "options": ["Paris", "London", "Berlin", "Madrid"],
+    "answer": "Paris"
 }]
